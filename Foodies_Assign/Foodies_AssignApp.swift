b0280f2c1ddee160b4bfd018a03ab2b41b61b2a1
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2024-10-19 12:24:46 +0900

    Initial Commit

---
+++ Foodies_Assign/Foodies_AssignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foodies_AssignApp.swift
+//  Foodies_Assign
+//
+//  Created by Satoshi Mitsumori on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Foodies_AssignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
